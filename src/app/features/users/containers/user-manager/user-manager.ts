bc067eb58951ef6e1a6f6566f7430a60d91acb06
Alessandro Fava <alessandro.fava@avanade.com>
2025-08-20 15:24:09 +0200

    initial structure

---
+++ src/app/features/users/containers/user-manager/user-manager.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-manager',
+  imports: [],
+  templateUrl: './user-manager.html',
+  styleUrl: './user-manager.css'
+})
+export class UserManager {
+
+}
